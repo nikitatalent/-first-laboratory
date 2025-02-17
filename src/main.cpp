c43220146c3ef930d4d82c01e70e4ce608e94365
Nikita <nikitatalent@gmail.com>
2025-02-17 21:48:27 +0300

    fix: Update main output message in main branch

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,6 @@ int main() {
     int b = 5;
     int add_res = add(a, b);
     int sub_res = sub(a, b);
-    std::cout << add_res << ' ' << sub_res << std::endl;
+    std::cout << "Результаты сложения и вычитания: " << add_res << ' ' << sub_res << std::endl;
     return 1;
 }
